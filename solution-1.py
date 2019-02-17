c137c3afea844dc0187e9aa4274180465eb38c53
Neil Moran <G00376338@gmit.ie>
2019-02-17 23:27:55 +0000

    First working version of solution-1.py

--- solution-1.py
+++ solution-1.py
@@ -1,2 +1,20 @@
 #Neil Moran  
 #Solution 1 
+
+#The number is taken in as a string
+xString = input("Enter a positive integer: ")
+
+#This line converts the string variable to an integer
+x = int(xString)
+
+# y is an integer variable for the counter in the while loop
+y = 0
+
+#z is an integer variable for the calculated value
+z = 0
+
+while y <= x:
+    z = z + y
+    y = y + 1
+
+print(z)
