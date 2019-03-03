a226c5751cfaffbfb7d34839ec47f7f49e2361f5
Neil Moran <G00376338@gmit.ie>
2019-03-03 23:04:44 +0000

    Added postitive integer check & changed division to be display a whole number

--- collatz.py
+++ collatz.py
@@ -1,12 +1,19 @@
 #Neil Moran 21/02/2019
 #Solution to Q4 collatz.py
 
+#Prompts the user to enter a positive integer and assigns the value to x
 x = int(input("Please enter a positive integer: "))
+
+#This while loop checks the inputed value of x to ensure it is a positive integer.
+#If it isn't it prompts the user to try again.
+while (x < 1):
+	print("This is not a positive integer! Please try again")
+	x = int(input("Please enter a positive integer: "))
 
 while(x > 1):
   y = x % 2
   if y == 0:
-    x = x / 2
+    x = x // 2
     print(x)
   else: 
     x = (x * 3) + 1
