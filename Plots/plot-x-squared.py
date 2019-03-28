8dfaa6d424ff7d1677450966008c84ea5d097756
Neil Moran <G00376338@gmit.ie>
2019-03-28 22:10:55 +0000

    Solution 1 to Q10 plotting the function of x when each value is squared from ranges from 0-4

--- Plots/plot-x-squared.py
+++ Plots/plot-x-squared.py
@@ -1,9 +1,12 @@
 #Neil Moran 28th March 2019
 #Solution for Q10 plotting function x squared is in the range [0,4]
 
+#The matplotlib.pyplot is imported as plt
 import matplotlib.pyplot as plt
 
+#This is the value for x in an list
 x = [0, 1, 2, 3, 4]
+#This is the new list for the squared values of x written in to a new list twopwrx
 xsqr = []
 y = 0
 
