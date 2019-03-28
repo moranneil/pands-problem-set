f6549f3aa3fe6991d553185d8ce6611ead3ddeb6
Neil Moran <G00376338@gmit.ie>
2019-03-28 10:24:38 +0000

    Added rounding to 1 decmal place for the answer & added code to check for a positive number

--- squareroot.py
+++ squareroot.py
@@ -3,13 +3,19 @@
 #Taken from Ian McLoughlin video lecture on 13th March 2019
 
 rootof = float(input("Please enter a positive number: "))
-print(rootof)
+
+#This while loop checks the inputed value of 'rootof' to ensure it is a positive number.
+#If it isn't it prompts the user to try again.
+while (rootof < 0.1):
+  print("This is not a positive number! Please try again")
+  rootof = float(input("Please enter a positive number: "))
+
+#This is the guess of the estimated value of the square root of the number  
 est = 2
 
 while abs((est * est) - rootof) > 0.1:
   est -= ((est * est) - rootof) / (2 * est)
-
-round(est,2)
-print(est)
-
-print(f"The square root of {rootof} is approx. {est}.")
+#This variable 'roundest' rounds the variable number 'est' to be one decimal place
+roundest = round(est,1)
+#Prints the original value and the rounded answer to the screen
+print(f"The square root of {rootof} is approx. {roundest}.")
