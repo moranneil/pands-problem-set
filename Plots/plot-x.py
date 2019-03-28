2b8e0bb8a00ef4082b671ff9f85c87af981d89d0
Neil Moran <G00376338@gmit.ie>
2019-03-28 22:07:36 +0000

    Solution 1 to Q10 plotting the function of x when each value is ranges from 0-4

---
+++ Plots/plot-x.py
@@ -0,0 +1,11 @@
+#Neil Moran 28th March 2019
+#Solution for Q10 plotting function x when x is in the range [0,4]
+
+import matplotlib.pyplot as plt
+
+x = [0, 1, 2, 3, 4]
+
+plt.plot(x)
+plt.xlabel('x  = range [0-4]')
+plt.ylabel('Function x')
+plt.show()
