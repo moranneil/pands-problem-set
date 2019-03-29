59e8e0e9d5144d98d3f0db726623c8f85b71dd14
Neil Moran <G00376338@gmit.ie>
2019-03-29 23:16:33 +0000

    Added comments to squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -1,5 +1,5 @@
 #Neil Moran 04/03/2019
-#Solution to Q7 secondstring.py
+#Solution to Q7 squareroot.py
 #Taken from Ian McLoughlin video lecture on 13th March 2019
 
 rootof = float(input("Please enter a positive number: "))
@@ -12,8 +12,9 @@ while (rootof < 0.1):
 
 #This is the guess of the estimated value of the square root of the number  
 est = 2
-
+#The while loop runs when the absolute value of the (est squared) - (the given number 'rootof') and checks its greater that 0.1, if a more accurate approximation is needed then 0.1 needs to be smaller eg .001
 while abs((est * est) - rootof) > 0.1:
+  #((est squared) - rootof))/(2 * est) is subtracted from est, this is repeated until  the while loop evaluation above is false
   est -= ((est * est) - rootof) / (2 * est)
 #This variable 'roundest' rounds the variable number 'est' to be one decimal place
 roundest = round(est,1)
