36ae7c786cb2c36067f58bfb9ad3383b28a4cb78
Neil Moran <G00376338@gmit.ie>
2019-03-29 11:52:12 +0000

    Corrected typo error in program

--- begins-with-t.py
+++ begins-with-t.py
@@ -6,7 +6,7 @@ import time
 
 #Day is a string variable when the function time.strftime("%w") from the time module is evaluated using the directive %w, 
 # "%w" returns the weekday as a decimal number Sun(0), Mon(1), Tue(2), Wed(3), Thur(4), Fri(5), Sat(6), Sun(7). 
-day= time.strftime("%w")
+day = time.strftime("%w")
 
 #The if statement evaluate the value of the string variable 'day' from the time module function time.strftime("%w"), if the value of day is either Tue(2) or Thur(4)
 #it prints "Yes - today begins with a T.", if the value of 'day' is not 2 or 4 the else in the if statement prints "No - today does not begin with a T."
